21bdd8a686dd28cd37faeb4e49dcb622d5dadb97
justine1109 <jampi_240000000765@uic.edu.ph>
2025-05-23 16:29:48 +0800

    Update sort.java

--- sort.java
+++ sort.java
@@ -2,22 +2,52 @@ import java.util.Arrays;
 
 public class BubbleSortNameList {
     public static void main(String[] args) {
-        // Array of names
+        // Original array of names
         String[] names = {"Zara", "Anna", "John", "Mike", "Bella"};
 
-        // Bubble Sort algorithm
-        for (int i = 0; i < names.length - 1; i++) {
-            for (int j = 0; j < names.length - i - 1; j++) {
-                if (names[j].compareToIgnoreCase(names[j + 1]) > 0) {
-                    // Swap names[j] and names[j + 1]
-                    String temp = names[j];
-                    names[j] = names[j + 1];
-                    names[j + 1] = temp;
+        // --- Bubble Sort ---
+        String[] bubbleSorted = names.clone();
+        for (int i = 0; i < bubbleSorted.length - 1; i++) {
+            for (int j = 0; j < bubbleSorted.length - i - 1; j++) {
+                if (bubbleSorted[j].compareToIgnoreCase(bubbleSorted[j + 1]) > 0) {
+                    // Swap
+                    String temp = bubbleSorted[j];
+                    bubbleSorted[j] = bubbleSorted[j + 1];
+                    bubbleSorted[j + 1] = temp;
                 }
             }
         }
+        System.out.println("Sorted using Bubble Sort:    " + Arrays.toString(bubbleSorted));
 
-        // Display sorted names using Arrays.toString()
-        System.out.println("Sorted names: " + Arrays.toString(names));
+        // --- Selection Sort ---
+        String[] selectionSorted = names.clone();
+        for (int i = 0; i < selectionSorted.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < selectionSorted.length; j++) {
+                if (selectionSorted[j].compareToIgnoreCase(selectionSorted[minIndex]) < 0) {
+                    minIndex = j;
+                }
+            }
+            // Swap
+            String temp = selectionSorted[minIndex];
+            selectionSorted[minIndex] = selectionSorted[i];
+            selectionSorted[i] = temp;
+        }
+        System.out.println("Sorted using Selection Sort: " + Arrays.toString(selectionSorted));
+
+        // --- Insertion Sort ---
+        String[] insertionSorted = names.clone();
+        for (int i = 1; i < insertionSorted.length; i++) {
+            String key = insertionSorted[i];
+            int j = i - 1;
+
+            // Move elements of insertionSorted[0..i-1] that are greater than key
+            while (j >= 0 && insertionSorted[j].compareToIgnoreCase(key) > 0) {
+                insertionSorted[j + 1] = insertionSorted[j];
+                j = j - 1;
+            }
+            insertionSorted[j + 1] = key;
+        }
+        System.out.println("Sorted using Insertion Sort: " + Arrays.toString(insertionSorted));
     }
 }
