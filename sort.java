b2429c058eddbf89f12316d1cb6c1c7c371bc365
justine1109 <jampi_240000000765@uic.edu.ph>
2025-05-23 15:21:49 +0800

    Add files via upload

---
+++ sort.java
@@ -0,0 +1,24 @@
+public class BubbleSortNameList {
+    public static void main(String[] args) {
+        // Array of names
+        String[] names = {"Zara", "Anna", "John", "Mike", "Bella"};
+
+        // Bubble Sort algorithm
+        for (int i = 0; i < names.length - 1; i++) {
+            for (int j = 0; j < names.length - i - 1; j++) {
+                if (names[j].compareToIgnoreCase(names[j + 1]) > 0) {
+                    // Swap names[j] and names[j + 1]
+                    String temp = names[j];
+                    names[j] = names[j + 1];
+                    names[j + 1] = temp;
+                }
+            }
+        }
+
+        // Display sorted names
+        System.out.println("Sorted names:");
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
